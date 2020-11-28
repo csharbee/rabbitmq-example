f377420b063764cf1f0bddc830ac55322dd1643a
ibrahimztrk61 <42765883+ibrahimztrk61@users.noreply.github.com>
2020-11-28 23:42:29 +0300

    Dead letter config added.

--- src/main/java/com/ibrahim/rabbitmqexample/config/RabbitmqConfiguration.java
+++ src/main/java/com/ibrahim/rabbitmqexample/config/RabbitmqConfiguration.java
@@ -4,8 +4,6 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,9 +20,24 @@ public class RabbitmqConfiguration {
     @Value("${sr.rabbit.exchange.name}")
     private String exchangeName;
 
+    @Value("${sr.rabbit.dead.letter.queue}")
+    private String deadLetterQueue;
+
+    @Value("${sr.rabbit.dead.letter.exchange}")
+    private String deadLetterExchange;
+
+    @Value("${sr.rabbit.dead.letter.routing}")
+    private String deadLetterRouting;
+
     @Bean
     public Queue queue() {
         return new Queue(queueName);
+    }
+
+
+    @Bean
+    Queue deadLetterQueue() {
+        return new Queue(deadLetterQueue);
     }
 
     @Bean
@@ -33,8 +46,20 @@ public class RabbitmqConfiguration {
     }
 
     @Bean
+    public DirectExchange deadLetterDirectExchange() {
+        return new DirectExchange(deadLetterExchange);
+    }
+
+    @Bean
     public Binding binding(final Queue queue, final DirectExchange directExchange) {
         return BindingBuilder.bind(queue).to(directExchange).with(routingName);
     }
+
+    @Bean
+    public Binding deadLetterBinding(final Queue deadLetterQueue, final DirectExchange deadLetterDirectExchange) {
+        return BindingBuilder.bind(deadLetterQueue).to(deadLetterDirectExchange).with(deadLetterRouting);
+    }
+
+
 
 }
